2d301dfca10070bf7248ce39ccda8b336371f81d
Michel César <httpmichel15cesar@gmail.com>
2026-07-10 23:57:28 +0100

    iniciando o curso o guanabara

---
+++ ex001/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-AO">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first Exercício</title>
+</head>
+
+<body>
+
+    <h1>
+        <?php
+        echo "Hello, World! \u{1F30D}";
+
+        ?>
+    </h1>
+
+</body>
+
+</html>
